e760bfd56606e953dfcce0a74b66a90cd43bd839
derv1shd <derv1shd@users.noreply.github.com>
2019-06-11 18:00:11 -0300

    Otro ejemplo de reescritura de XML.

---
+++ PreprocessXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PreprocessXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new StreamReader("Sample response - Pedidos.xml");
+            StringBuilder input = new StringBuilder(stream.ReadToEnd());
+
+            string pattern = @"<Posicion>[0-9]*</Posicion>";
+
+            foreach(Match match in Regex.Matches(input.ToString(), pattern))
+            {
+                string newTag = match.Value.Replace("Posicion", "Posicion_Id");
+                input.Replace(match.Value, newTag);
+            }
+        }
+    }
+}
