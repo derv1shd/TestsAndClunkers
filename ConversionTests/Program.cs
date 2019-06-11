636859a00fae97fc2ec7be95daa4538fa74d0896
derv1shd <derv1shd@users.noreply.github.com>
2019-06-11 10:47:10 -0300

    Algunas pruebas de conversión

---
+++ ConversionTests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceData = "1.127.969,89";
+            CultureInfo culture = new CultureInfo("es-CL", false);
+            Double doubleValue;
+            if (Double.TryParse(sourceData, NumberStyles.Currency, culture, out doubleValue))
+                Console.WriteLine(doubleValue.ToString());
+            else
+                Console.WriteLine("Error");
+        }
+    }
+}
